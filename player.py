968fcad770d20d09b4a246dad41c882d2d4bfcd9
Garrett Kucinski <garrettryan@doejo.com>
2017-02-09 23:26:10 -0600

    commit of all progress, validated user input

--- player.py
+++ player.py
@@ -10,41 +10,44 @@ class Player(object):
     def get_ship_coordinates(self, ship_name, ship_length):
         """Ask input from user on where to put each ship."""
         prompt = "{}, Choose a position for the {} ({} spaces):"
-
         ship_coordinates = input(prompt.format(
             self.name, ship_name, ship_length))
+
+        return ship_coordinates
+
+    def are_valid_coordinates(self, ship_coordinates):
+        """Check if ship coordinates are valid."""
+        column, row = ship_coordinates
 
         if len(ship_coordinates) != 2:
             print('{}, {} is not a valid entry.'
                   'Please enter a column and a row, ex. "h4"'
                   .format(self.name, ship_coordinates))
-            return self.get_ship_coordinates(ship_name, ship_length)
-        else:
-            column, row = tuple(ship_coordinates)
-
-        if column not in constants.VALID_LETTERS:
+            return False
+        elif column not in constants.VALID_LETTERS:
             print("{}, {} is not a valid column valid lettered columns are {}"
                   .format(self.name, column, constants.VALID_LETTERS))
-            return self.get_ship_coordinates(ship_name, ship_length)
+            return False
         elif int(row) > constants.BOARD_SIZE:
             print('{}, {} is not a valid row, valid rows are numbered 1-{}'
                   .format(self.name, constants.BOARD_SIZE))
-            return self.get_ship_coordinates(ship_name, ship_length)
+            return False
         else:
-            return ship_coordinates
+            return True
 
     def get_ship_orientation(self):
         """Method to get ship orientation input from player."""
         ship_orientation = input("{}, Please choose an orientation"
                                  " [V]ertical or [H]orizontal): "
                                  .format(self.name)).lower()
+        return ship_orientation
 
+    def is_valid_orientation(self, ship_orientation):
+        """Check for valid orientation input."""
         if ship_orientation not in 'hv':
             print('{} is not a valid orientation,'
                   'please enter either "h" or "v"'.format(ship_orientation))
             return self.get_ship_orientation()
-        else:
-            return ship_orientation
 
         # space_for_ship = (constants.BOARD_SIZE - constants.VALID_LETTERS.index(ship_coordinates)) + ship_length
 
@@ -55,10 +58,16 @@ class Player(object):
         passed in and place each one simultaneously updating the players board.
         """
         for ship_name, ship_length in ships:
-            ship_coordinates = self.get_ship_coordinates(
+            if self.are_valid_coordinates(self.get_ship_coordinates(ship_name, ship_length)):
+                ship_coordinates = self.get_ship_coordinates(
                                                         ship_name, ship_length)
+            else:
+                return self.get_ship_coordinates(ship_name, ship_length)
 
-            ship_orientation = self.get_ship_orientation()
+            if self.is_valid_orientation(self.get_ship_orientation()):
+                ship_orientation = self.get_ship_orientation()
+            else:
+                return self.get_ship_orientation()
 
             ship_data = {"orientation":  ship_orientation,
                          "position": ship_coordinates, "ship_name": ship_name,
